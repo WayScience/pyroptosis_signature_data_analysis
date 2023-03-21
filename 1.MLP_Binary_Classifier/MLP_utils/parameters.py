19e5d7403a35b98c4fe10ea0bbeb626347f4d81f
MikeLippincott <1michaell2017@gmail.com>
2023-03-21 09:57:07 -0600

    "add config file"

--- 1.MLP_Binary_Classifier/MLP_utils/parameters.py
+++ 1.MLP_Binary_Classifier/MLP_utils/parameters.py
@@ -1,3 +1,4 @@
+"""Parametes for Machine Learning Model Training and Testing"""
 from dataclasses import dataclass
 
 import torch
@@ -7,27 +8,26 @@ import torch
 class Parameters:
     """Class for keeping constants"""
 
-    def __init__(self):
-        # Constants
-        # Subset option yes or no? if no SUBSET_NUMBER won't be used
-        self.SUBSET_OPTION = True
+    # Constants
+    # Subset option yes or no? if no SUBSET_NUMBER won't be used
+    DATA_SUBSET_OPTION = True
 
-        # number of rows to subset main df for
-        # in this casse each row is 1 cell
-        self.SUBSET_NUMBER = 1500
+    # number of rows to subset main df for
+    # in this case each row is 1 cell
+    DATA_SUBSET_NUMBER = 1500
 
-        # Batch of data to load into data loader (1 is equivalent to 1 row or 1 cell in this case)
-        self.BATCH_SIZE = 100
+    # Batch of data to load into data loader (1 is equivalent to 1 row or 1 cell in this case)
+    BATCH_SIZE = 100
 
-        # number of epochs to use for model optimization
-        self.OPTIM_EPOCHS = 100
-        # number of trials to use for model optimization
-        self.N_TRIALS = 100
+    # number of epochs to use for model optimization
+    OPTIM_EPOCHS = 100
+    # number of trials to use for model optimization
+    N_TRIALS = 50
 
-        # number of epochs to use for optimized model
-        self.TRAIN_EPOCHS = 1000
+    # number of epochs to use for optimized model
+    TRAIN_EPOCHS = 1000
 
-        # device use
-        # defined as global for use in the optimizer function and training function
-        # global DEVICE
-        self.DEVICE = torch.device("cuda" if (torch.cuda.is_available()) else "cpu")
+    # device use
+    # defined as global for use in the optimizer function and training function
+    # global DEVICE
+    DEVICE = torch.device("cuda" if (torch.cuda.is_available()) else "cpu")

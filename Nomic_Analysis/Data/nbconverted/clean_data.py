2425cd6bba2916b9fcc0307881dd04e36f0a252c
MikeLippincott <1michaell2017@gmail.com>
2023-04-20 15:38:06 -0600

    PBMC and SH-SY5Y analysis and merge fix

--- Nomic_Analysis/Data/nbconverted/clean_data.py
+++ Nomic_Analysis/Data/nbconverted/clean_data.py
@@ -6,30 +6,17 @@
 
 import pathlib
 
-import iplot
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import plotly
-import plotly.graph_objs as go
 import plotly_express as px
 import seaborn as sns
 import umap
-from plotly.offline import download_plotlyjs, init_notebook_mode, iplot, plot
-from sklearn import preprocessing
 
 # ## Clean Data from Nomic
 
 # In[2]:
-
-
-# max rows displayed
-pd.set_option("display.max_rows", 1000)
-# max columns displayed
-pd.set_option("display.max_columns", 1000)
-
-
-# In[3]:
 
 
 nELISA_data_input_path = pathlib.Path("./raw/nELISA_Data_UCA1_2023.04.11.csv")
@@ -40,18 +27,21 @@ nELISA_data_all = pd.read_csv(nELISA_data_input_path)
 MetaData = pd.read_csv(MetaData_input_path)
 
 
-# In[4]:
+# In[3]:
 
 
 # Change the 'A1' cell format to 'A01' format
 position = []
 for i in MetaData["position"].astype(str):
-    position.append(i[:1] + f"{0}" + i[1:])
+    if len(i) < 3:
+        position.append(i[:1] + f"{0}" + i[1:])
+    else:
+        position.append(i)
 MetaData["position"] = position
 MetaData.head()
 
 
-# In[5]:
+# In[4]:
 
 
 # Change column names
@@ -74,7 +64,7 @@ nELISA_data_all["plate_position"] = (
 nELISA_data_all.replace(regex=[" and "], value="_", inplace=True)
 
 
-# In[6]:
+# In[5]:
 
 
 # Seperate df out by plate
@@ -85,7 +75,7 @@ MetaData_plate_430418_430419 = MetaData.loc[
 MetaData_plate_430420 = MetaData.loc[MetaData["plate_barcode"] == "430420"]
 
 
-# In[7]:
+# In[6]:
 
 
 # seperate out by plate
@@ -98,14 +88,15 @@ nELISA_data_all_plate_430420 = nELISA_data_all.loc[
 ]
 
 
-# In[8]:
+# In[7]:
 
 
 # Merge the two dataframes for plate 430420 via concat because pandas is being a pain
-plate_430420 = pd.concat(
-    [MetaData_plate_430420, nELISA_data_all_plate_430420],
-    axis=1,
-    join="inner",
+plate_430420 = pd.merge(
+    MetaData_plate_430420,
+    nELISA_data_all_plate_430420,
+    on="plate_position",
+    how="inner",
 )
 
 # remove empty wells and qc_fail
@@ -119,14 +110,15 @@ plate_430420 = plate_430420[
 plate_430420.shape
 
 
-# In[9]:
+# In[8]:
 
 
 # Merge the two dataframes for plate 430418_430419
-plate_430418_430419 = pd.concat(
-    [MetaData_plate_430420, nELISA_data_all_plate_430420],
-    axis=1,
-    join="inner",
+plate_430418_430419 = pd.merge(
+    MetaData_plate_430418_430419,
+    nELISA_data_all_plate_430418_430419,
+    on="plate_position",
+    how="inner",
 )
 
 # remove empty wells and qc_fail
@@ -139,12 +131,33 @@ plate_430418_430419 = plate_430418_430419[
 plate_430418_430419.shape
 
 
+# In[9]:
+
+
+# seperate out by cell type
+plate_430420_PBMC = plate_430420.loc[plate_430420["cell_type"] == "PBMC"]
+plate_430420_PBMC.shape
+
+plate_430420_SH_SY5Y = plate_430420.loc[plate_430420["cell_type"] == "SH-SY5Y"]
+plate_430420_SH_SY5Y.shape
+
+
 # In[10]:
 
 
 # define output paths
-nELISA_plate_430420 = pathlib.Path("./clean/nELISA_plate_430420.csv")
-nELISA_430418_430419 = pathlib.Path("./clean/nELISA_plate_430418_430419.csv")
+nELISA_plate_430420_path = pathlib.Path("./clean/Plate2/nELISA_plate_430420.csv")
+nELISA_plate_430420_PBMC_path = pathlib.Path(
+    "./clean/Plate2/nELISA_plate_430420_PBMC.csv"
+)
+nELISA_plate_430420_SH_SY5Y_path = pathlib.Path(
+    "./clean/Plate2/nELISA_plate_430420_SH_SY5Y.csv"
+)
+nELISA_430418_430419_path = pathlib.Path(
+    "./clean/Plate1/nELISA_plate_430418_430419.csv"
+)
 # write to csv
-plate_430420.to_csv(nELISA_plate_430420, index=False)
-plate_430418_430419.to_csv(nELISA_430418_430419, index=False)
+plate_430420.to_csv(nELISA_plate_430420_path, index=False)
+plate_430420.to_csv(nELISA_plate_430420_PBMC_path, index=False)
+plate_430420.to_csv(nELISA_plate_430420_SH_SY5Y_path, index=False)
+plate_430418_430419.to_csv(nELISA_430418_430419_path, index=False)

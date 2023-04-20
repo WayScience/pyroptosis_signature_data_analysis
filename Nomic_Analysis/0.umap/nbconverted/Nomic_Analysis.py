9204b4679f837955693bd75cd215eb81e2251a8c
MikeLippincott <1michaell2017@gmail.com>
2023-04-20 11:23:24 -0600

    full umap_analysis

--- Nomic_Analysis/0.umap/nbconverted/Nomic_Analysis.py
+++ Nomic_Analysis/0.umap/nbconverted/Nomic_Analysis.py
@@ -21,36 +21,75 @@ from sklearn import preprocessing
 # In[2]:
 
 
-nELISA_data_input_path = pathlib.Path("../Data/clean/nELISA_Data_UCA1_2023.04.11.csv")
-MetaData_input_path = pathlib.Path("../Data/clean/Metadata_UCA1_2023.04.11.csv")
+def umap_graph(df: pd.DataFrame, color: str, save_name: str):
+    """plot umap graph
 
-nELISA_data_all = pd.read_csv(nELISA_data_input_path)
-MetaData = pd.read_csv(MetaData_input_path)
+    Parameters
+    ----------
+    df : pd.DataFrame
+        pandas dataframe with umap coordinates
+    color : str
+        column to color points by
+    save_name : str
+        name of file to be saved
+    """
+
+    # plot UMAP
+    fig_2d = px.scatter(
+        df,
+        x="umap_1",
+        y="umap_2",
+        color=color,
+        labels={"color": "Cell Type"},
+        title="UMAP projection of the nELISA data",
+    ).update_layout(xaxis_title="UMAP_1", yaxis_title="UMAP_2")
+
+    fig_2d.update_traces(marker={"size": 12})
+    fig_2d.show()
+
+    # save UMAP
+    # fig_2d.write_html(f"{save_name}.html")
+    fig_2d.write_image(f"{save_name}.png")
 
 
 # In[3]:
 
 
-# nELISA_data = pd.merge(MetaData, nELISA_data_all, left_on='plate_position',right_on='plate_position', how='inner')
-nELISA_data = pd.merge(MetaData, nELISA_data_all, on="plate_position", how="right")
-nELISA_data_values = nELISA_data.filter(like="NSU", axis=1)
-nELISA_data_values = nELISA_data_values.astype("float")
-print(nELISA_data_values.shape)
-# pd.merge(MetaData, nELISA_data_all, on='plate_position',how='left').shape
-nELISA_data_values.head()
+nELISA_plate_430418_430419_path = pathlib.Path(
+    "../Data/clean/nELISA_plate_430418_430419.csv"
+)
+nELISA_plate_430420_path = pathlib.Path("../Data/clean/nELISA_plate_430420.csv")
+manual_clusters_path = pathlib.Path("../Data/Manual_Treatment_Clusters.csv")
+
+nELISA_plate_430418_430419 = pd.read_csv(nELISA_plate_430418_430419_path)
+nELISA_plate_430420 = pd.read_csv(nELISA_plate_430420_path)
+manual_clusters = pd.read_csv(manual_clusters_path)
 
 
 # In[4]:
 
 
-max_values = nELISA_data_values.max()
-nELISA_data_values_sensor_max_norm = nELISA_data_values.div(max_values)
-nELISA_data_values_sensor_max_norm.head()
+# select data only columns and make floats
+nELISA_data_values = nELISA_plate_430420.filter(like="NSU", axis=1)
+nELISA_data_values = nELISA_data_values.astype("float")
+nELISA_data_values.head()
 
 
 # In[5]:
 
 
+# normalize data via max value in each column
+max_values = nELISA_data_values.max()  # find max value in each column
+nELISA_data_values_sensor_max_norm = nELISA_data_values.div(
+    max_values
+)  # divide each value in each column by max value in that column
+nELISA_data_values_sensor_max_norm.head()
+
+
+# In[6]:
+
+
+# print mean and stdev of first data column before and after normalization to check normalization
 print(f"NSU nELISA mean of Activin A: {nELISA_data_values['Activin A [NSU]'].mean()}")
 print(f"NSU nELISA STDEV of Activin A: {nELISA_data_values['Activin A [NSU]'].std()}")
 
@@ -61,105 +100,51 @@ print(
     f"NSU sensor normalized nELISA STDEV of Activin A: {nELISA_data_values_sensor_max_norm['Activin A [NSU]'].std()}"
 )
 
-# normalize the data min max
-scaler = preprocessing.MinMaxScaler().fit(nELISA_data_values)
-nELISA_data_min_max = scaler.transform(nELISA_data_values)
-
-nELISA_data_min_max = pd.DataFrame(
-    nELISA_data_min_max, columns=nELISA_data_values.columns
-)
-print(
-    f"NSU Min Max normalized nELISA mean of Activin A: {nELISA_data_min_max['Activin A [NSU]'].mean()}"
-)
-print(
-    f"NSU Min Max normalized nELISA STDEV of Activin A: {nELISA_data_min_max['Activin A [NSU]'].std()}"
-)
-
-# normalize the data standard scaler
-scaler = preprocessing.StandardScaler().fit(nELISA_data_values)
-nELISA_data_standard = scaler.transform(nELISA_data_values)
-nELISA_data_standard = pd.DataFrame(
-    nELISA_data_standard, columns=nELISA_data_values.columns
-)
-
-print(
-    f"NSU Standard Scalar normalized nELISA mean of Activin A: {nELISA_data_standard['Activin A [NSU]'].mean()}"
-)
-print(
-    f"NSU Standard Scalar normalized nELISA STDEV of Activin A: {nELISA_data_standard['Activin A [NSU]'].std()}"
-)
-
-
-# In[6]:
-
-
-nELISA_data_values.head()
-
 
 # In[7]:
 
 
-nELISA_data_values_sensor_max_norm.head()
+# rename columns to remove special character "/"
+nELISA_plate_430420.columns = nELISA_plate_430420.columns.str.replace("/", "_")
+
+# set umap parameters
+umap_params = umap.UMAP(
+    n_neighbors=6,
+    min_dist=0.8,
+    n_components=2,
+    metric="cosine",
+    spread=1.1,
+    init="random",
+    random_state=0,
+)
+
+# fit and transform data for umap
+proj_2d = umap_params.fit_transform(nELISA_data_values_sensor_max_norm)
+
+# add umap coordinates to dataframe of metadata and raw data
+nELISA_plate_430420["umap_1"] = proj_2d[:, 0]
+nELISA_plate_430420["umap_2"] = proj_2d[:, 1]
+
+# add manual clusters columns to dataframe
+nELISA_plate_430420 = pd.merge(
+    nELISA_plate_430420, manual_clusters, on=("inducer1", "inhibitor"), how="inner"
+)
+
+# define output paths
+nELISA_plate_430420_out_path = pathlib.Path("./results/nELISA_plate_430420_umap.csv")
+# write to csv
+nELISA_plate_430420.to_csv(nELISA_plate_430420_out_path, index=False)
 
 
 # In[8]:
 
 
-nELISA_data_min_max.head()
-
-
-# In[9]:
-
-
-nELISA_data_standard.head()
-
-
-# In[10]:
-
-
-def umap_graph(df, df1, save_name):
-    # UMAP parameters
-    umap_df = umap.UMAP(
-        n_neighbors=6,
-        min_dist=0.8,
-        n_components=2,
-        metric="cosine",
-        spread=1.1,
-        init="random",
-        random_state=0,
+nELISA_plate_430420 = nELISA_plate_430420[
+    nELISA_plate_430420.columns.drop(list(nELISA_plate_430420.filter(regex="pgML")))
+]
+for col in nELISA_plate_430420.columns:
+    umap_graph(
+        nELISA_plate_430420,
+        f"{col}",
+        f"./figures/nELISA_Normalized_UMAP_{col}",
     )
-
-    # fit and transform data
-    proj_2d = umap_df.fit_transform(df)
-
-    # plot UMAP
-    fig_2d = px.scatter(
-        proj_2d,
-        x=0,
-        y=1,
-        color=df1.cell_type,
-        labels={"color": "Cell Type"},
-        title="UMAP projection of the nELISA data",
-    ).update_layout(xaxis_title="UMAP_1", yaxis_title="UMAP_2")
-
-    fig_2d.update_traces(marker={"size": 15})
-    fig_2d.show()
-
-    # save UMAP
-    fig_2d.write_html(f"{save_name}.html")
-    fig_2d.write_image(f"{save_name}.png")
-
-
-umap_graph(nELISA_data_values, nELISA_data, "./figures/nELISA_data_values_raw")
-umap_graph(
-    nELISA_data_values_sensor_max_norm,
-    nELISA_data,
-    "./figures/nELISA_data_values_max_sensor_noramalized",
-)
-umap_graph(nELISA_data_min_max, nELISA_data, "./figures/nELISA_data_min_max_normalized")
-umap_graph(
-    nELISA_data_standard, nELISA_data, "./figures/nELISA_data_standard_normalized"
-)
-
-
-# In[ ]:

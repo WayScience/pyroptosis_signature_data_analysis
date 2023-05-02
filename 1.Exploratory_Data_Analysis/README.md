40a8ae5aa91e3f39b0b2b73fc51cfdb287820fb8
MikeLippincott <1michaell2017@gmail.com>
2023-05-02 16:40:58 -0600

    cell per well vis and umap for plate2

--- 1.Exploratory_Data_Analysis/README.md
+++ 1.Exploratory_Data_Analysis/README.md
@@ -1,1 +1,7 @@
 # This folder hosts jupyter notebooks for exploratory data analysis (EDA) for multiple 'waves' of data. See this repo's main page for a link to the Interstellar preprocessing page.
+
+```Exploratory_Data_Analysis.ipynb```
+is for clustering of cells across all features. The goal is to aim to see if treatments cluster or not.
+```cell_count_analysis.ipynb``` is for qc analysis to assess if there are cell count drop offs for a given treatment.
+
+All figures are saved under `Figures` folder.
--- 1.Exploratory_Data_Analysis/nbconverted/cell_count_analysis.py
+++ 1.Exploratory_Data_Analysis/nbconverted/cell_count_analysis.py
@@ -25,11 +25,13 @@ from utils.utils import df_stats, plot_features_all_cluster
 # In[2]:
 
 
-path = pathlib.Path("../../Extracted_Features_(CSV_files)/feature_df_sc_norm.parquet")
+path = pathlib.Path(
+    "../../Extracted_Features_(CSV_files)/SHSY5Y_preprocessed_df_sc_norm.parquet"
+)
 df = pq.read_table(path).to_pandas()
 
 
-# In[8]:
+# In[3]:
 
 
 sns.barplot(
@@ -44,11 +46,16 @@ plt.ylabel("Number of single cells")
 plt.xlabel("Treatment")
 plt.xticks(rotation=45)
 plt.title("Number of single cells per treatment")
+
+plt.savefig(
+    "Figures/cell_counts_plate2/Number_of_single_cells_per_treatment.png",
+    bbox_inches="tight",
+)
 plt.show()
 plt.close()
 
 
-# In[10]:
+# In[4]:
 
 
 for i in df["Metadata_Treatment"].unique():
@@ -65,8 +72,9 @@ for i in df["Metadata_Treatment"].unique():
     plt.ylabel("Number of single cells")
     plt.xticks(rotation=45)
     plt.title(f"Number of single cells per {i}")
+    plt.savefig(
+        f"Figures/cell_counts_plate2/Number_of_single_cells_per_{i}.png",
+        bbox_inches="tight",
+    )
     plt.show()
     plt.close()
-
-
-# In[ ]:

026054700628bdc6ec84a977200c93912df9b234
MikeLippincott <1michaell2017@gmail.com>
2023-07-11 18:26:00 -0600

    updated bash scripts

--- 0.Preprocess_Data/pre-processing.sh
+++ 0.Preprocess_Data/pre-processing.sh
@@ -2,7 +2,7 @@
 
 conda activate Interstellar
 
-jupyter nbconvert --to=script --FilesWriter.build_directory=scripts *.ipynb
-
 papermill 1.preprocessing.ipynb 1.preprocessing.ipynb -p celltype "SHSY5Y"
-# papermill 1.preprocessing.ipynb 1.preprocessing.ipynb -p celltype "PBMC"
+papermill 1.preprocessing.ipynb 1.preprocessing.ipynb -p celltype "PBMC"
+
+jupyter nbconvert --to=script --FilesWriter.build_directory=scripts *.ipynb

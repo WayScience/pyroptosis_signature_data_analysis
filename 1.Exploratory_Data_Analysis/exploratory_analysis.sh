0f014866246ff0e16178ce6314fa813a30f12a15
MikeLippincott <1michaell2017@gmail.com>
2023-07-11 18:26:00 -0600

    updated bash scripts

--- 1.Exploratory_Data_Analysis/exploratory_analysis.sh
+++ 1.Exploratory_Data_Analysis/exploratory_analysis.sh
@@ -5,4 +5,7 @@ conda activate Interstellar
 papermill 0.cell_count_analysis.ipynb 0.cell_count_analysis.ipynb -p celltype "SHSY5Y"
 papermill 1.umap_analysis_plate2.ipynb 1.umap_analysis_plate2.ipynb -p celltype "SHSY5Y"
 
+papermill 0.cell_count_analysis.ipynb 0.cell_count_analysis.ipynb -p celltype "PBMC"
+papermill 1.umap_analysis_plate2.ipynb 1.umap_analysis_plate2.ipynb -p celltype "PBMC"
+
 jupyter nbconvert --to=script --FilesWriter.build_directory=scripts *.ipynb
--- 3.sc_Morphology_Linear_Modeling/sc_linear_modeling.sh
+++ 3.sc_Morphology_Linear_Modeling/sc_linear_modeling.sh
@@ -7,6 +7,11 @@ papermill 1b.fit_linear_model_2beta.ipynb 1b.fit_linear_model_2beta.ipynb -p cel
 papermill 1c.fit_linear_model_3beta.ipynb 1c.fit_linear_model_3beta.ipynb -p celltype 'SHSY5Y'
 papermill 1d.fit_linear_model_4beta.ipynb 1d.fit_linear_model_4beta.ipynb -p celltype 'SHSY5Y'
 
+papermill 1a.fit_linear_model_1beta.ipynb 1a.fit_linear_model_1beta.ipynb -p celltype 'PBMC'
+papermill 1b.fit_linear_model_2beta.ipynb 1b.fit_linear_model_2beta.ipynb -p celltype 'PBMC'
+papermill 1c.fit_linear_model_3beta.ipynb 1c.fit_linear_model_3beta.ipynb -p celltype 'PBMC'
+papermill 1d.fit_linear_model_4beta.ipynb 1d.fit_linear_model_4beta.ipynb -p celltype 'PBMC'
+
 # copnvert to scripts after running papermill and conver r notebooks to scripts prior to running
 jupyter nbconvert --to=script --FilesWriter.build_directory=scripts *.ipynb
 
@@ -14,3 +19,8 @@ Rscript scripts/2a.Linear_Modeling_Visualization.r --celltype 'SHSY5Y'
 Rscript scripts/2b.Linear_Modeling_Visualization.r --celltype 'SHSY5Y'
 Rscript scripts/2c.Linear_Modeling_Visualization.r --celltype 'SHSY5Y'
 Rscript scripts/2d.Linear_Modeling_Visualization.r --celltype 'SHSY5Y'
+
+Rscript scripts/2a.Linear_Modeling_Visualization.r --celltype 'PBMC'
+Rscript scripts/2b.Linear_Modeling_Visualization.r --celltype 'PBMC'
+Rscript scripts/2c.Linear_Modeling_Visualization.r --celltype 'PBMC'
+Rscript scripts/2d.Linear_Modeling_Visualization.r --celltype 'PBMC'

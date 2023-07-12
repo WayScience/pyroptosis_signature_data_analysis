c9c11cd9c07f77a33dd0e44c6221d7cb4ac7ee73
MikeLippincott <1michaell2017@gmail.com>
2023-07-11 20:37:54 -0600

    sbatch updates

--- 1.Exploratory_Data_Analysis/exploratory_analysis.sh
+++ 1.Exploratory_Data_Analysis/exploratory_analysis.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks=10
+#SBATCH --ntasks=1
 #SBATCH --mem=256G
-#SBATCH --partition=amilan
-#SBATCH --qos=normal
-#SBATCH --time=12:00:00
+#SBATCH --partition=amem
+#SBATCH --qos=mem
+#SBATCH --time=24:00:00
 #SBATCH --output=sample-%j.out
 
 module load anaoconda

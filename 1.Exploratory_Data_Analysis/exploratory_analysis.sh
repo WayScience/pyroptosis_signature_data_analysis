8dfd5c3308a84d74ef494cd57c321ce1d7c21358
MikeLippincott <1michaell2017@gmail.com>
2023-08-06 10:44:19 -0600

    update exploratory analysis

--- 1.Exploratory_Data_Analysis/exploratory_analysis.sh
+++ 1.Exploratory_Data_Analysis/exploratory_analysis.sh
@@ -12,13 +12,11 @@ module load anaconda
 
 conda activate Interstellar
 
-papermill 0.cell_count_analysis.ipynb 0.cell_count_analysis.ipynb -p celltype "SHSY5Y"
 papermill 1.umap_analysis_plate2.ipynb 1.umap_analysis_plate2.ipynb -p celltype "SHSY5Y"
-papermill 2.Corelation_between_nomic_and_sc.ipynb 2.Corelation_between_nomic_and_sc.ipynb -p celltype "SHSY5Y"
+papermill 4.cell_count_analysis.ipynb 0.cell_count_analysis.ipynb -p celltype "SHSY5Y"
 
-papermill 0.cell_count_analysis.ipynb 0.cell_count_analysis.ipynb -p celltype "PBMC"
 papermill 1.umap_analysis_plate2.ipynb 1.umap_analysis_plate2.ipynb -p celltype "PBMC"
-papermill 2.Corelation_between_nomic_and_sc.ipynb 2.Corelation_between_nomic_and_sc.ipynb -p celltype "PBMC"
+papermill 4.cell_count_analysis.ipynb 0.cell_count_analysis.ipynb -p celltype "PBMC"
 
 
 jupyter nbconvert --to=script --FilesWriter.build_directory=scripts *.ipynb

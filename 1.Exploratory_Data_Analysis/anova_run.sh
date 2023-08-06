f59d4e9d0b4e4f02d552fab9b13410d0694ca508
MikeLippincott <1michaell2017@gmail.com>
2023-08-06 09:47:46 -0600

    prep anova for PBMC HPC run

--- 1.Exploratory_Data_Analysis/anova_run.sh
+++ 1.Exploratory_Data_Analysis/anova_run.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=300G
+#SBATCH --partition=amem
+#SBATCH --qos=mem
+#SBATCH --time=24:00:00
+#SBATCH --output=sample-%
 
+module load anaconda
 
-treatment1s=(DMSO_0.100_DMSO_0.025)
-treatment2s=(LPS_100.000_DMSO_0.025)
-treatment3s=(Thapsigargin_10.000_DMSO_0.025)
-cell_types=(SHSY5Y)
+conda activate Interstellar
+
+treatment1s=( DMSO_0.100_DMSO_0.025 )
+treatment2s=( LPS_100.000_DMSO_0.025 LPS_10.000_DMSO_0.025 LPS_1.000_DMSO_0.025 LPS_0.100_DMSO_0.025 LPS_0.010_DMSO_0.025)
+treatment3s=( Thapsigargin_10.000_DMSO_0.025 Thapsigargin_1.000_DMSO_0.025 H202_100_Z-VAD-FMK_100 H202_100_DMSO_0.025)
+cell_types=( SHSY5Y PBMC )
 
 for treatment1 in ${treatment1s[@]}; do
     for treatment2 in ${treatment2s[@]}; do
         for treatment3 in ${treatment3s[@]}; do
             for cell_type in ${cell_types[@]}; do
-            echo "treatment1: $treatment1 treatment2: $treatment2 treatment3: $treatment3 cell_type: $cell_type"
+                echo "treatment1: $treatment1 treatment2: $treatment2 treatment3: $treatment3 cell_type: $cell_type"
                 papermill 5.anova.ipynb \
                 5.anova.ipynb \
                 -p cell_type SHSY5Y \

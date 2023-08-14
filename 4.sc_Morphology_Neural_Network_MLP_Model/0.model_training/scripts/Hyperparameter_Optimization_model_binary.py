14ad529a38e1ac8ed454c6120fdce233d1818ea3
Mike Lippincott <58147848+MikeLippincott@users.noreply.github.com>
2023-08-14 09:11:52 -0600

    Update 4.sc_Morphology_Neural_Network_MLP_Model/0.model_training/scripts/Hyperparameter_Optimization_model_binary.py
    Co-authored-by: Erik Serrano <31600622+axiomcura@users.noreply.github.com>

--- 4.sc_Morphology_Neural_Network_MLP_Model/0.model_training/scripts/Hyperparameter_Optimization_model_binary.py
+++ 4.sc_Morphology_Neural_Network_MLP_Model/0.model_training/scripts/Hyperparameter_Optimization_model_binary.py
@@ -489,7 +489,7 @@ for SHUFFLE in [True, False]:
 
     # Call visualization function
     # calling the testing function and outputing list values of tested model
-    if params.MODEL_TYPE == "Multi_Class" or params.MODEL_TYPE == "Regression":
+    if any(model_type == params.MODEL_TYPE for model_type in ["Multi_Class", "Regression"]):
         confusion_matrix_df = results_output(
             y_pred_list,
             df_values_Y,

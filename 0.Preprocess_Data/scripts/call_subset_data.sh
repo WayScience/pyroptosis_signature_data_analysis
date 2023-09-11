99c4a28d5fbc2e0b99e5a97f3ecaa41c2670befe
MikeLippincott <1michaell2017@gmail.com>
2023-09-11 16:04:18 -0600

    split_index_on_HPC

---
+++ 0.Preprocess_Data/scripts/call_subset_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=256G
+#SBATCH --partition=amem
+#SBATCH --qos=mem
+#SBATCH --time=24:00:00
+#SBATCH --output=sample-%j.out
+
+module load anaconda
+
+conda activate Interstellar
+python 2.subset_PBMC_data_for_testing.py
+
+echo "DONE"

21836cd009b6c00de36cf27be45c77b0f5388fc4
MikeLippincott <1michaell2017@gmail.com>
2023-09-14 08:24:06 -0600

    updated testing scripts

--- 6.bulk_Morphology_Elastic_Network/2.test_model/scripts/test_model_call.sh
+++ 6.bulk_Morphology_Elastic_Network/2.test_model/scripts/test_model_call.sh
@@ -13,27 +13,13 @@ module load anaconda
 
 conda activate Interstellar
 
-# get the array of cytokiens
-filename="../../0.split_data/cytokine_list/cytokine_list.txt"
-# read all lines of the file to an array
-readarray -t cytokine_array < $filename
-
 shuffles=(True False)
 cell_types=( SHSY5Y PBMC )
-# calculate the number of jobs
-num_jobs=$(( ${#shuffles[@]} * ${#cell_types[@]} * ${#cytokine_array[@]} ))
-#SBATCH --array=1-$num_jobs%8
 
-# calculate the number of jobs
-job_id=$((SLURM_ARRAY_TASK_ID - 1))
-shuffle_idx=$((job_id % ${#shuffles[@]}))
-cell_type_idx=$(((job_id / ${#shuffles[@]}) % ${#cell_types[@]}))
-cytokine_idx=$(((job_id / ${#shuffles[@]} / ${#cell_types[@]}) % ${#cytokine_array[@]}))
-
-shuffle=${shuffles[$shuffle_idx]}
-cell_type=${cell_types[$cell_type_idx]}
-cytokine=${cytokine_array[$cytokine_idx]}
-
-command="python 1.test_regression_multi_output.py"
-
-$command --cell_type "$cell_type" --shuffle "$shuffle" --cytokine "$cytokine"
+for shuffle in "${shuffles[@]}"; do
+    for cell_type in "${cell_types[@]}"; do
+        echo $cell_type $shuffle
+        python 1.test_regression_multi_output.py--cell_type "$cell_type" --shuffle "$shuffle"
+    done
+done
+echo "Done"

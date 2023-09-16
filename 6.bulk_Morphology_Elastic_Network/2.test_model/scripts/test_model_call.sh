85ea90b02f841b99311fc924579208929eac6957
MikeLippincott <1michaell2017@gmail.com>
2023-09-16 14:04:18 -0600

    HPC prep for testing enet

--- 6.bulk_Morphology_Elastic_Network/2.test_model/scripts/test_model_call.sh
+++ 6.bulk_Morphology_Elastic_Network/2.test_model/scripts/test_model_call.sh
@@ -2,24 +2,33 @@
 # This script is used to train the regression models for the elastic network
 
 #SBATCH --nodes=1
-#SBATCH --ntasks=8
+#SBATCH --ntasks=4
 #SBATCH --mem=500G
 #SBATCH --partition=amem
 #SBATCH --qos=mem
-#SBATCH --time=24:00:00
+#SBATCH --time=72:00:00
 #SBATCH --output=sample-%j.out
+#SBATCH --array=1-4%4
 
 module load anaconda
 
 conda activate Interstellar
 
-shuffles=(True False)
+shuffles=( True False )
 cell_types=( SHSY5Y PBMC )
 
-for shuffle in "${shuffles[@]}"; do
-    for cell_type in "${cell_types[@]}"; do
-        echo $cell_type $shuffle
-        python 1.test_regression_multi_output.py--cell_type "$cell_type" --shuffle "$shuffle"
-    done
-done
+# calculate the number of jobs
+job_id=$((SLURM_ARRAY_TASK_ID - 1))
+cell_type_idx=$((job_id % ${#cell_types[@]}))
+shuffle_idx=$((job_id / ${#cell_types[@]}))
+
+cell_type=${cell_types[$cell_type_idx]}
+shuffle=${shuffles[$shuffle_idx]}
+
+echo "Cell type: $cell_type" "Shuffle: $shuffle"
+
+command="python 1.test_regression_multi_output.py"
+
+$command --cell_type "$cell_type" --shuffle "$shuffle"
+
 echo "Done"

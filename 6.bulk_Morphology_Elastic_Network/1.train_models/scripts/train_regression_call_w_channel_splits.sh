8ed608cc9c52ce708df36f539e20256348a1d393
MikeLippincott <1michaell2017@gmail.com>
2023-11-06 17:02:05 -0700

    attempt 1 at paralell on slurm

--- 6.bulk_Morphology_Elastic_Network/1.train_models/scripts/train_regression_call_w_channel_splits.sh
+++ 6.bulk_Morphology_Elastic_Network/1.train_models/scripts/train_regression_call_w_channel_splits.sh
@@ -3,9 +3,11 @@
 
 #SBATCH --nodes=1
 #SBATCH --ntasks=20
-#SBATCH --partition=amem
-#SBATCH --qos=mem
-#SBATCH --time=72:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=150G
+#SBATCH --partition=normal
+#SBATCH --qos=normal
+#SBATCH --time=10:00:00
 #SBATCH --output=sample-%j.out
 #SBATCH --array=1-750%20
 
@@ -22,22 +24,47 @@ readarray -t cytokine_array < $filename
 channel_filename="../../0.split_data/cytokine_list/channel_splits.txt"
 readarray -t channel_array < $channel_filename
 
+run_parallel() {
+    local job_id="$1"
+    local shuffle_idx=$((job_id % ${#shuffles[@]}))
+    local cell_type_idx=$(((job_id / ${#shuffles[@]}) % ${#cell_types[@]}))
+    local cytokine_idx=$(((job_id / ${#shuffles[@]} / ${#cell_types[@]}) % ${#cytokine_array[@]}))
+    local channel_idx=$(((job_id / ${#shuffles[@]} / ${#cell_types[@]} / ${#cytokine_array[@]}) % ${#channel_array[@]}))
 
-shuffles=( True False )
-cell_types=( SHSY5Y PBMC )
-# calculate the number of jobs
-# calculate the number of jobs
-job_id=$((SLURM_ARRAY_TASK_ID - 1))
-shuffle_idx=$((job_id % ${#shuffles[@]}))
-cell_type_idx=$(((job_id / ${#shuffles[@]}) % ${#cell_types[@]}))
-cytokine_idx=$(((job_id / ${#shuffles[@]} / ${#cell_types[@]}) % ${#cytokine_array[@]}))
-channel_idx=$(((job_id / ${#shuffles[@]} / ${#cell_types[@]} / ${#cytokine_array[@]}) % ${#channel_array[@]}))
+    local shuffle=${shuffles[$shuffle_idx]}
+    local cell_type=${cell_types[$cell_type_idx]}
+    local cytokine=${cytokine_array[$cytokine_idx]}
 
-shuffle=${shuffles[$shuffle_idx]}
-cell_type=${cell_types[$cell_type_idx]}
-cytokine=${cytokine_array[$cytokine_idx]}
-channel=${channel_array[$channel_idx]}
+    local command="python 1.train_regression_multi_output.py"
 
-command="python 1.train_regression_multi_output.py"
+    # use parallel to run the command for each channel
+    parallel -j 4 $command --cell_type "$cell_type" --cytokine "$cytokine" --shuffle "$shuffle" --data {} ::: "${channel_array[@]}"
+}
+# export the function so it can be used by parallel
+export -f run_parallel
 
-$command --cell_type "$cell_type" --cytokine "$cytokine" --shuffle "$shuffle" --data "$channel"
+# run the parallel function for the current array index
+run_parallel "$SLURM_ARRAY_TASK_ID"
+
+# shuffles=( True False )
+# cell_types=( SHSY5Y PBMC )
+# # calculate the number of jobs
+# # calculate the number of jobs
+# job_id=$((SLURM_ARRAY_TASK_ID - 1))
+# shuffle_idx=$((job_id % ${#shuffles[@]}))
+# cell_type_idx=$(((job_id / ${#shuffles[@]}) % ${#cell_types[@]}))
+# cytokine_idx=$(((job_id / ${#shuffles[@]} / ${#cell_types[@]}) % ${#cytokine_array[@]}))
+# channel_idx=$(((job_id / ${#shuffles[@]} / ${#cell_types[@]} / ${#cytokine_array[@]}) % ${#channel_array[@]}))
+
+# shuffle=${shuffles[$shuffle_idx]}
+# cell_type=${cell_types[$cell_type_idx]}
+# cytokine=${cytokine_array[$cytokine_idx]}
+# channel=${channel_array[$channel_idx]}
+
+# command="python 1.train_regression_multi_output.py"
+
+# $command --cell_type "$cell_type" --cytokine "$cytokine" --shuffle "$shuffle" --data "$channel"
+
+
+
+

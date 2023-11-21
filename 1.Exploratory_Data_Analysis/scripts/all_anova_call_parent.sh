3c2416fcf356fb147982514911657695222a5599
MikeLippincott <1michaell2017@gmail.com>
2023-11-21 08:16:21 -0700

    attempt4

--- 1.Exploratory_Data_Analysis/scripts/all_anova_call_parent.sh
+++ 1.Exploratory_Data_Analysis/scripts/all_anova_call_parent.sh
@@ -7,11 +7,11 @@
 #SBATCH --time=168:00:00
 
 
-module load anaconda
+# module load anaconda
 
-conda activate Interstellar_python
+# conda activate Interstellar_python
 
-jupyter nbconvert --to=script --FilesWriter.build_directory=. ../notebooks/*.ipynb
+# jupyter nbconvert --to=script --FilesWriter.build_directory=. ../notebooks/*.ipynb
 
 
 CELL_TYPE="PBMC"
@@ -22,27 +22,18 @@ readarray -t features < $filename
 
 echo "Feature: $feature" "Cell type: $CELL_TYPE"
 
-# Loop until the number of jobs is less than 25
-while true; do
-    # Count the number of running and pending jobs
-    total_jobs=$(squeue -h -u $USER | wc -l)
-
-    # If the total number of jobs is less than 1000, submit the next job
-    if [ $total_jobs -lt 30 ]; then
-        # Get the next item from the list
-        feature=${features[$SLURM_ARRAY_TASK_ID-1]}
-
-        # Check if there are still items to process
-        if [ -n "$feature" ]; then
-            echo "Submitting job for item: $feature"
-            sbatch all_anova_call_child.sh $feature $CELL_TYPE
-        else
-            echo "All items processed. Exiting."
+# loop through the array
+for feature in "${features[@]}"; do
+    # echo "Feature: $feature" "Cell type: $CELL_TYPE"
+    # check if the number of slurms is less than 30
+    while true; do
+        NUM_SLURMS=$(squeue -u $USER | wc -l)
+        if [ "$NUM_SLURMS" -lt 30 ]; then
+            echo "Feature: $feature" "Cell type: $CELL_TYPE"
+            sbatch anova_call_test.sh $feature $CELL_TYPE
             break
+        else
+            sleep 10
         fi
-    else
-        echo "Waiting for job slots to become available..."
-        sleep 30  # Adjust the sleep duration as needed
-    fi
+    done
 done
-

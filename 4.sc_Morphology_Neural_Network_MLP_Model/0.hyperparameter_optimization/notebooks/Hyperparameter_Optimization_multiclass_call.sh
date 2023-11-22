b3c9d84d4e602550794830380c69c1c112121aa4
MikeLippincott <1michaell2017@gmail.com>
2023-11-22 11:44:46 -0700

    attempt4

--- 4.sc_Morphology_Neural_Network_MLP_Model/0.hyperparameter_optimization/notebooks/Hyperparameter_Optimization_multiclass_call.sh
+++ 4.sc_Morphology_Neural_Network_MLP_Model/0.hyperparameter_optimization/notebooks/Hyperparameter_Optimization_multiclass_call.sh
@@ -2,29 +2,31 @@
 
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --mem=400G
+#SBATCH --mem=500G
 #SBATCH --partition=amem
 #SBATCH --qos=mem
-#SBATCH --time=48:00:00
+#SBATCH --time=72:00:00
 #SBATCH --output=sample-%j.out
+#SBATCH --array=1-2%2
 
 module load anaconda
 
-conda activate Interstellar
+conda activate Interstellar_python
 
 cell_types=( SHSY5Y PBMC )
 model_names=( MultiClass_MLP )
 
-for cell_type in "${cell_types[@]}"; do
-    for model_name in "${model_names[@]}"; do
-        papermill \
-        Hyperparameter_Optimization_model_multiclass.ipynb \
-        Hyperparameter_Optimization_model_multiclass.ipynb \
-        -p CELL_TYPE $cell_type \
-        -p MODEL_NAME $model_name
-    done
-done
-
 jupyter nbconvert --to=script --FilesWriter.build_directory=../scripts *.ipynb
+
+job_id=$((SLURM_ARRAY_TASK_ID - 1))
+model_name_idx=$((job_id % ${#model_names[@]}))
+cell_type_idx=$(((job_id / ${#model_names[@]}) % ${#cell_types[@]}))
+
+cell_type=${cell_types[$cell_type_idx]}
+model_name=${model_names[$model_name_idx]}
+
+command="python Hyperparameter_Optimization_model_multiclass.py"
+
+$command --cell_type "$cell_type" --model_name "$model_name"
 
 echo "Done"

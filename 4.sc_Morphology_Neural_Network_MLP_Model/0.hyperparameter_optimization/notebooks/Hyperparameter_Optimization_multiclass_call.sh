289fd4aa9c3712002c887598d9adb2c727369b2b
MikeLippincott <1michaell2017@gmail.com>
2023-11-22 07:52:01 -0700

    hyperparameters

---
+++ 4.sc_Morphology_Neural_Network_MLP_Model/0.hyperparameter_optimization/notebooks/Hyperparameter_Optimization_multiclass_call.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=400G
+#SBATCH --partition=amem
+#SBATCH --qos=mem
+#SBATCH --time=48:00:00
+#SBATCH --output=sample-%j.out
+
+module load anaconda
+
+conda activate Interstellar
+
+cell_types=( SHSY5Y PBMC )
+model_names=( MultiClass_MLP )
+
+for cell_type in "${cell_types[@]}"; do
+    for model_name in "${model_names[@]}"; do
+        papermill \
+        Hyperparameter_Optimization_model_binary.ipynb \
+        Hyperparameter_Optimization_model_binary.ipynb \
+        -p CELL_TYPE $cell_type \
+        -p MODEL_NAME $model_name
+    done
+done
+
+jupyter nbconvert --to=script --FilesWriter.build_directory=../scripts *.ipynb
+
+echo "Done"

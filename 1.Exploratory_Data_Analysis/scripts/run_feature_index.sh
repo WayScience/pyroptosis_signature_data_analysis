04dba85b1c485eb6951ea750ac48302af46097ce
MikeLippincott <1michaell2017@gmail.com>
2023-12-24 10:11:22 -0700

    fixed anova list

--- 1.Exploratory_Data_Analysis/scripts/run_feature_index.sh
+++ 1.Exploratory_Data_Analysis/scripts/run_feature_index.sh
@@ -7,13 +7,13 @@
 #SBATCH --output=sample-%j.out
 #SBATCH --time=24:00:00
 
-# module load anaconda
+module load anaconda
 
-# conda activate Interstellar_python
+conda activate Interstellar_python
 
 echo "Running feature index for all cell types"
 
-# python 8.0_create_feature_index.py --cell_type "PBMC"
+python 8.0_create_feature_index.py --cell_type "PBMC"
 
 python 8.0_create_feature_index.py --cell_type "SHSY5Y"
 

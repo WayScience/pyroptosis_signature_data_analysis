98ee1e835dde3e8be1bbd822d696e99173829d41
MikeLippincott <1michaell2017@gmail.com>
2024-03-04 12:17:18 -0700

    first run

---
+++ 10.IDR_metadata/notebooks/scripts/1.data_aggregation.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <span style="color:red; font-family:Helvetica Neue, Helvetica, Arial, sans-serif; font-size:2em;">An Exception was encountered at '<a href="#papermill-error-cell">In [4]</a>'.</span>
+
+# This noteboook aggregates the data from the previous notebooks and creates the final dataset for the analysis barring the data are aggregated in the analysis.
+
+# In[1]:
+
+
+import pathlib
+
+import numpy as np
+import pandas as pd
+
+# In[2]:
+
+
+# Parameters
+cell_type = "PBMC"
+aggregation = True
+nomic = True
+
+
+# In[3]:
+
+
+if aggregation and nomic:
+    aggregated_data_path = pathlib.Path(
+        f"../data/{cell_type}_preprocess_sc_norm_no_fs_aggregated_nomic.parquet"
+    )
+elif not aggregation and nomic:
+    aggregated_data_path = pathlib.Path(
+        f"../data/{cell_type}_preprocess_sc_norm_no_fs_nomic.parquet"
+    )
+elif aggregation and not nomic:
+    aggregated_data_path = pathlib.Path(
+        f"../data/{cell_type}_preprocess_sc_norm_no_fs_aggregated.parquet"
+    )
+elif not aggregation and not nomic:
+    pass
+else:
+    raise ValueError("Wrong parameters")
+
+
+# <span id="papermill-error-cell" style="color:red; font-family:Helvetica Neue, Helvetica, Arial, sans-serif; font-size:2em;">Execution using papermill encountered an exception here and stopped:</span>
+
+# In[4]:
+
+
+path = pathlib.Path(f"../data/{cell_type}_preprocess_sc_norm_no_fs.parquet")
+
+data_df = pd.read_parquet(path)
+
+data_df.head()
+
+if nomic:
+    # import nomic data
+    nomic_df_path = pathlib.Path(
+        f"../2.Nomic_nELISA_Analysis/Data/clean/Plate2/nELISA_plate_430420_{cell_type}_clean.parquet"
+    )
+    df_nomic = pd.read_parquet(nomic_df_path)
+
+    # drop columns that contain [pgML]
+    df_nomic = df_nomic.drop(
+        columns=[col for col in df_nomic.columns if "[pgML]" in col]
+    )
+    # drop first 25 columns (metadata that does not contain metadata in the title)
+    df_nomic = df_nomic.drop(columns=df_nomic.columns[3:25])
+    df_nomic = df_nomic.drop(columns=df_nomic.columns[0:2])
+elif not nomic:
+    pass
+else:
+    raise ValueError("Nomic data not imported")
+
+
+# In[ ]:
+
+
+# subset each column that contains metadata
+metadata = data_df.filter(regex="Metadata")
+
+# get all columns that are not metadata except for metadata_Well
+data = data_df.drop(metadata.columns, axis=1)
+
+# get the metadata_Well column
+metadata_well = metadata[
+    ["Metadata_Well", "oneb_Metadata_Treatment_Dose_Inhibitor_Dose"]
+]
+
+data_df = pd.merge(data, metadata_well, left_index=True, right_index=True)
+
+
+# In[ ]:
+
+
+if nomic:
+    df_nomic.drop(
+        columns=[
+            "Treatment",
+            "Dose",
+            "twob_Treatment_Dose_Inhibitor_Dose",
+            "threeb_Treatment_Dose_Inhibitor_Dose",
+            "fourb_Treatment_Dose_Inhibitor_Dose",
+        ],
+        inplace=True,
+    )
+
+
+# In[ ]:
+
+
+if aggregation and nomic:
+
+    # subset each column that contains metadata
+    metadata = data_df.filter(regex="Metadata")
+    data_df = data_df.drop(metadata.columns, axis=1)
+    data_df = pd.concat([data_df, metadata["Metadata_Well"]], axis=1)
+    # groupby well and take mean of each well
+    data_df = data_df.groupby("Metadata_Well").mean()
+    # drop duplicate rows in the metadata_well column
+    metadata = metadata.drop_duplicates(subset=["Metadata_Well"])
+    # get the metadata for each well
+    data_df = pd.merge(
+        data_df, metadata, left_on="Metadata_Well", right_on="Metadata_Well"
+    )
+    data_df = pd.merge(
+        data_df,
+        df_nomic,
+        left_on=["Metadata_Well", "oneb_Metadata_Treatment_Dose_Inhibitor_Dose"],
+        right_on=["position_x", "oneb_Treatment_Dose_Inhibitor_Dose"],
+    )
+    data_df = data_df.drop(columns=["position_x"])
+    # drop all metadata columns
+    labeled_data = data_df["oneb_Metadata_Treatment_Dose_Inhibitor_Dose"]
+    data_x = data_df.drop(metadata.columns, axis=1)
+    # set path to save the data
+    aggregated_data_path = pathlib.Path(
+        f"../data/{cell_type}_preprocessed_sc_norm_aggregated_nomic.parquet"
+    )
+
+
+elif aggregation and not nomic:
+    # get metadata columns
+    metadata = data_df.filter(regex="Metadata")
+    data_df = data_df.drop(metadata.columns, axis=1)
+    metadata
+    data_df = pd.concat([data_df, metadata], axis=1)
+    # groupby well and take mean of each well
+    data_df = data_df.groupby(
+        ["Metadata_Well", "oneb_Metadata_Treatment_Dose_Inhibitor_Dose"]
+    ).mean()
+    # # drop duplicate rows in the metadata_well column
+    metadata = metadata.drop_duplicates(subset=["Metadata_Well"])
+    # # get the metadata for each well
+    # # set path to save the data
+    aggregated_data_path = pathlib.Path(
+        f"../data/{cell_type}_preprocessed_sc_norm_aggregated.parquet"
+    )
+    # reset the index
+    data_df = data_df.reset_index()
+
+elif not aggregation and nomic:
+    data_df = pd.merge(
+        data_df,
+        df_nomic,
+        left_on=["Metadata_Well", "oneb_Metadata_Treatment_Dose_Inhibitor_Dose"],
+        right_on=["position_x", "oneb_Treatment_Dose_Inhibitor_Dose"],
+    )
+    data_df = data_df.drop(columns=["position_x"])
+    # set path to save the data
+    aggregated_data_path = pathlib.Path(
+        f"../data/{cell_type}_preprocessed_sc_norm_with_nomic.parquet"
+    )
+elif aggregation == False and nomic == False:
+    pass
+else:
+    raise ValueError("Wrong parameters nomica and/or aggregation not defined")
+
+
+# In[ ]:
+
+
+# save the data
+data_df.to_parquet(aggregated_data_path)
---
+++ 10.IDR_metadata/notebooks/scripts/10.preprocess_profiles.py
@@ -0,0 +1,266 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Parameters
+cell_type = "PBMC"
+
+
+# In[2]:
+
+
+import pathlib
+
+import numpy as np
+import pandas as pd
+import papermill as pm
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+# In[3]:
+
+
+# Parameters
+cell_type = "SHSY5Y"
+
+
+# In[4]:
+
+
+# Define inputs
+feature_file = pathlib.Path(f"../../data/{cell_type}_sc_norm.parquet")
+feature_df = pd.read_parquet(feature_file)
+
+
+# In[5]:
+
+
+# replace all " " with "_" in all values of the dataframe
+feature_df = feature_df.replace(to_replace=" ", value="_", regex=True)
+
+
+# In[6]:
+
+
+# remove uM in each row of the Metadata_inducer1_concentration column
+feature_df["Metadata_inducer1_concentration"] = feature_df[
+    "Metadata_inducer1_concentration"
+].str.replace("µM", "")
+
+
+# In[7]:
+
+
+feature_df["Metadata_inducer1_concentration"].unique()
+
+
+# In[8]:
+
+
+# define output file path
+feature_df_out_path = pathlib.Path(
+    f"../../data/{cell_type}_preprocess_sc_norm_no_fs.parquet"
+)
+
+
+# In[9]:
+
+
+print(feature_df.shape)
+feature_df.head()
+
+
+# In[10]:
+
+
+# removing costes features as they behave with great variance across all data
+feature_df = feature_df.drop(feature_df.filter(regex="Costes").columns, axis=1)
+print(feature_df.shape)
+feature_df.head()
+
+
+# In[11]:
+
+
+# replacing '/' in treatment dosage column to avoid errors in file interpolation including such strings
+feature_df = feature_df.replace(to_replace="/", value="_per_", regex=True)
+
+
+# In[12]:
+
+
+# replace nan values with 0
+
+columns_to_fill = [
+    "Metadata_inducer1_concentration",
+    "Metadata_inducer2_concentration",
+    "Metadata_inhibitor_concentration",
+]
+feature_df[columns_to_fill].fillna(0, inplace=True)
+
+
+# In[13]:
+
+
+# replace all None values with 0
+feature_df["Metadata_inducer1_concentration"].fillna(0, inplace=True)
+
+
+# In[14]:
+
+
+# create a list of columns to be converted to float
+col_list = [
+    "Metadata_inducer1_concentration",
+    "Metadata_inducer2_concentration",
+    "Metadata_inhibitor_concentration",
+]
+# loop through the list and convert each column to float
+for i in col_list:
+    feature_df[i] = feature_df[i].apply(
+        lambda x: f"{float(x):.3f}" if float(x) != 0 else float(x)
+    )
+
+
+# #### Combine Inducer1 and Inducer2 into one column
+
+# In[15]:
+
+
+# treatment column merge
+conditions = [
+    (feature_df["Metadata_inducer2"].isnull()),
+    feature_df["Metadata_inducer2"].notnull(),
+]
+
+results = [
+    (feature_df["Metadata_inducer1"]).astype(str),
+    (
+        feature_df["Metadata_inducer1"]
+        + "_"
+        + feature_df["Metadata_inducer2"].astype(str)
+    ),
+]
+feature_df["Metadata_Treatment"] = np.select(condlist=conditions, choicelist=results)
+
+
+# dose column merge
+results = [
+    (
+        feature_df["Metadata_inducer1_concentration"].astype(str)
+        + "_"
+        + feature_df["Metadata_inducer1_concentration_unit"].astype(str)
+    ),
+    (
+        feature_df["Metadata_inducer1_concentration"].astype(str)
+        + "_"
+        + feature_df["Metadata_inducer1_concentration_unit"].astype(str)
+        + "_"
+        + feature_df["Metadata_inducer2_concentration"].astype(str)
+        + "_"
+        + feature_df["Metadata_inducer2_concentration_unit"].astype(str)
+    ),
+]
+feature_df["Metadata_Dose"] = np.select(condlist=conditions, choicelist=results)
+
+
+# ## N Beta Column condition generation
+# columns generated to used for linear modeling where terms separated by '__' will be a beta coefficient
+
+# In[16]:
+
+
+# one beta of inudcer1, inducer1 concentration, inhibitor, and inhibitor concentration all as 1 beta term
+feature_df["oneb_Metadata_Treatment_Dose_Inhibitor_Dose"] = (
+    feature_df["Metadata_Treatment"]
+    + "_"
+    + feature_df["Metadata_Dose"].astype(str)
+    # + "_"
+    # + feature_df['Metadata_inducer1_concentration_unit'].astype(str)
+    + "_"
+    + feature_df["Metadata_inhibitor"].astype(str)
+    + "_"
+    + feature_df["Metadata_inhibitor_concentration"].astype(str)
+    + "_"
+    + feature_df["Metadata_inhibitor_concentration_unit"].astype(str)
+).astype(str)
+
+
+# two beta of inducer1, inhibitor, and inhibitor concentration all as 1 beta term + inducer1 concentration as 2nd beta term
+feature_df["twob_Metadata_Treatment_Dose_Inhibitor_Dose"] = (
+    feature_df["Metadata_Treatment"]
+    + "_"
+    + feature_df["Metadata_inhibitor"].astype(str)
+    + "_"
+    + feature_df["Metadata_inhibitor_concentration"].astype(str)
+    + "__"
+    + feature_df["Metadata_Dose"].astype(str)
+).astype(str)
+
+# three beta of inducer 1 as 1 beta term, inducer1 concentration as 2nd beta term, inhibitor and inhibitor concentration as 3rd beta term
+feature_df["threeb_Metadata_Treatment_Dose_Inhibitor_Dose"] = (
+    feature_df["Metadata_Treatment"]
+    + "__"
+    + feature_df["Metadata_Dose"].astype(str)
+    + "__"
+    + feature_df["Metadata_inducer1_concentration_unit"].astype(str)
+    + "_"
+    + feature_df["Metadata_inhibitor"].astype(str)
+    + "_"
+    + feature_df["Metadata_inhibitor_concentration"].astype(str)
+).astype(str)
+
+# four beta of inducer 1 as 1 beta term, inducer1 concentration as 2nd beta term, inhibitor as 3rd beta term, and inhibitor concentration as 4th beta term
+feature_df["fourb_Metadata_Treatment_Dose_Inhibitor_Dose"] = (
+    feature_df["Metadata_Treatment"]
+    + "__"
+    + feature_df["Metadata_Dose"].astype(str)
+    + "__"
+    + feature_df["Metadata_inducer1_concentration_unit"].astype(str)
+    + "_"
+    + feature_df["Metadata_inhibitor"].astype(str)
+    + "__"
+    + feature_df["Metadata_inhibitor_concentration"].astype(str)
+).astype(str)
+
+
+# In[17]:
+
+
+replacement_dict = {
+    "None": "0",
+    "µ": "u",
+    "nan": "0",
+}
+for pattern, replacement in replacement_dict.items():
+    print(pattern, replacement)
+    feature_df["oneb_Metadata_Treatment_Dose_Inhibitor_Dose"] = feature_df[
+        "oneb_Metadata_Treatment_Dose_Inhibitor_Dose"
+    ].replace(to_replace=str(pattern), value=str(replacement), regex=True)
+
+
+# In[18]:
+
+
+feature_df["oneb_Metadata_Treatment_Dose_Inhibitor_Dose"] = feature_df[
+    "oneb_Metadata_Treatment_Dose_Inhibitor_Dose"
+].str.replace("media_ctr_0.0_0_Media_ctr_0_0", "media_ctr_0.0_0_Media_ctr_0.0_0")
+
+
+# In[19]:
+
+
+# need to convert to strings to save as parquet
+# if the column is an object then convert it to a string
+for column in feature_df.columns:
+    if feature_df[column].dtype == "object":
+        feature_df[column] = feature_df[column].astype(str)
+
+
+# In[20]:
+
+
+# write to parquet file
+feature_df.to_parquet(feature_df_out_path)

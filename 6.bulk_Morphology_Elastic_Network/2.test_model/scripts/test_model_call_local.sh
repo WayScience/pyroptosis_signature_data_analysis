74d8e94f49fca9eee872a690517b95ac6b8b113b
Mike Lippincott <58147848+MikeLippincott@users.noreply.github.com>
2024-04-22 08:08:15 -0600

    Update 6.bulk_Morphology_Elastic_Network/2.test_model/scripts/test_model_call_local.sh
    Co-authored-by: Gregory Way <gregory.way@gmail.com>

--- 6.bulk_Morphology_Elastic_Network/2.test_model/scripts/test_model_call_local.sh
+++ 6.bulk_Morphology_Elastic_Network/2.test_model/scripts/test_model_call_local.sh
@@ -5,7 +5,7 @@
 
 conda activate Interstellar_python
 
-# get the array of cytokiens
+# get the array of cytokines
 filename="../../0.split_data/cytokine_list/cytokine_list.txt"
 # read all lines of the file to an array
 readarray -t cytokine_array < $filename

1a59c5022f02fa4a0cf08262599d576603bd1039
MikeLippincott <1michaell2017@gmail.com>
2024-10-03 15:29:48 -0600

    ready for alpine

--- 11.bulk_Morphology_Elastic_Network_LOCO/1.train_models/train_regression_call.sh
+++ 11.bulk_Morphology_Elastic_Network_LOCO/1.train_models/train_regression_call.sh
@@ -2,37 +2,39 @@
 # This script is used to train the regression models for the elastic network
 
 #SBATCH --nodes=1
-#SBATCH --ntasks=20
-#SBATCH --partition=amem
-#SBATCH --qos=mem
-#SBATCH --time=48:00:00
+#SBATCH --ntasks=50
+#SBATCH --partition=amilan
+#SBATCH --qos=normal
+#SBATCH --time=15:00
 #SBATCH --output=sample-%j.out
-#SBATCH --array=1-750%20
+#SBATCH --array=1-187%50
+
+# 1 cell type * 1 shuffle type * 187 cytokines = 187 per child script
+# run the array at 50 tasks per node at one node for 15 minutes
 
 module load anaconda
 
 conda activate Interstellar
 
-# get the array of cytokiens
+# get the args
+cell_type=$1
+shuffle=$2
+feature_columns=$3
+
+# get the array of cytokines
 filename="../../0.split_data/cytokine_list/cytokine_list.txt"
 # read all lines of the file to an array
 readarray -t cytokine_array < $filename
 
 jupyter nbconvert --to=script --FilesWriter.build_directory=. ../notebooks/*.ipynb
 
-shuffles=( True False )
-cell_types=( SHSY5Y PBMC )
 # calculate the number of jobs
 # calculate the number of jobs
 job_id=$((SLURM_ARRAY_TASK_ID - 1))
-shuffle_idx=$((job_id % ${#shuffles[@]}))
-cell_type_idx=$(((job_id / ${#shuffles[@]}) % ${#cell_types[@]}))
-cytokine_idx=$(((job_id / ${#shuffles[@]} / ${#cell_types[@]}) % ${#cytokine_array[@]}))
+cytokine_idx=$(((job_id % ${#cytokine_array[@]})))
 
-shuffle=${shuffles[$shuffle_idx]}
-cell_type=${cell_types[$cell_type_idx]}
 cytokine=${cytokine_array[$cytokine_idx]}
 
 command="python 1.train_regression_multi_output.py"
 
-$command --cell_type "$cell_type" --cytokine "$cytokine" --shuffle "$shuffle"
+$command --cell_type "$cell_type" --shuffle "$shuffle" --cytokine "$cytokine" --feature_columns "$feature_columns"
---
+++ 11.bulk_Morphology_Elastic_Network_LOCO/1.train_models/train_regression_call_parent.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# This script is used to train the regression models for the elastic network
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=16
+#SBATCH --partition=amilan
+#SBATCH --qos=long
+#SBATCH --time=72:00:00
+#SBATCH --output=sample-%j.out
+#SBATCH --array=1-128%16
+
+# 32 channel combination * 2 cell types * 2 shuffles = 128
+# run the array at 16 tasks per node at one node for 72 hours
+
+module load anaconda
+
+conda activate Interstellar
+
+# get the array of cytokiens
+feature_combination_file="../0.split_data/results/feature_combinations_PBMC.toml"
+
+# get all of the feature combinations
+# feature cominations array
+feature_combinations=()
+for combination in $(tomlq '. | keys' $feature_combination_file | jq -r '.[]'); do
+    echo $combination
+    echo tomlq -r ".$combination[]" $feature_combination_file
+    output=$(tomlq -r ".$combination[]" "$feature_combination_file")
+    python_list=$(echo "$output" | awk '{printf "%s", ($0 ~ /^[^"]*$/ ? "\""$0"\"" : $0) (NR==NF ? "" : ", ")}' | sed 's/^/[/; s/$/]/')
+    echo $python_list
+    feature_combinations+=( "$python_list" )
+done
+
+
+jupyter nbconvert --to=script --FilesWriter.build_directory=. ../notebooks/*.ipynb
+
+shuffles=( True False )
+cell_types=( SHSY5Y PBMC )
+# calculate the number of jobs
+# calculate the number of jobs
+job_id=$((SLURM_ARRAY_TASK_ID - 1))
+shuffle_idx=$((job_id % ${#shuffles[@]}))
+cell_type_idx=$(((job_id / ${#shuffles[@]}) % ${#cell_types[@]}))
+feature_combination_idx=$(((job_id / ${#shuffles[@]} / ${#cell_types[@]}) % ${#feature_combinations[@]}))
+
+shuffle=${shuffles[$shuffle_idx]}
+cell_type=${cell_types[$cell_type_idx]}
+feature_combination=${feature_combinations[$feature_combination_idx]}
+
+sbatch train_regression_call_parent.sh $cell_type $shuffle $feature_combination

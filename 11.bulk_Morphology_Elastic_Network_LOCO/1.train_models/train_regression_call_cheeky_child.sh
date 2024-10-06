995c13cfac8a0162bcc2eb972d67cf0c9451d5e7
MikeLippincott <1michaell2017@gmail.com>
2024-10-05 20:31:05 -0600

    testing checky-ness

---
+++ 11.bulk_Morphology_Elastic_Network_LOCO/1.train_models/train_regression_call_cheeky_child.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# This script is used to train the regression models for the elastic network
+
+#SBATCH --nodes=1
+#SBATCH --mem=250M
+#SBATCH --partition=amilan
+#SBATCH --qos=normal
+#SBATCH --time=1:30:00
+#SBATCH --output=sample_train-%j.out
+
+# 1 cell type * 1 shuffle type * 187 cytokines = 187 per child script
+# run the array at 50 tasks per node at one node for 15 minutes
+
+module load anaconda
+conda init bash
+conda activate Interstellar_python
+
+# get the args
+cell_type=$1
+shuffle=$2
+feature_combinations_key=$3
+
+# get the array of cytokines
+filename="../0.split_data/cytokine_list/cytokine_list.txt"
+# read all lines of the file to an array
+readarray -t cytokine_array < $filename
+
+jupyter nbconvert --to=script --FilesWriter.build_directory=./scripts/ ./notebooks/*.ipynb
+
+# calculate the number of jobs
+# calculate the number of jobs
+job_id=$((SLURM_ARRAY_TASK_ID - 1))
+cytokine_idx=$(((job_id % ${#cytokine_array[@]})))
+
+cytokine=${cytokine_array[$cytokine_idx]}
+
+cd scripts/
+
+command="python 1.train_regression_multi_output.py"
+
+echo "$cell_type $shuffle $cytokine"
+
+$command --cell_type "$cell_type" --shuffle "$shuffle" --cytokine "$cytokine" --feature_combinations_key "$feature_combinations_key"
+
+cd ../
+
+echo "Complete"
---
+++ 11.bulk_Morphology_Elastic_Network_LOCO/1.train_models/train_regression_call_cheeky_parent.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# This script is used to train the regression models for the elastic network
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --partition=amilan
+#SBATCH --qos=long
+#SBATCH --time=96:00:00
+#SBATCH --output=sample_parent-%j.out
+
+
+# 32 channel combination * 2 cell types * 2 shuffles * 187 cytokines = 23936
+
+module load anaconda
+
+conda init bash
+
+conda activate Interstellar_python
+
+# get the array of cytokiens
+feature_combination_key_file="../0.split_data/results/feature_combinations_keys.txt"
+# get the array of cytokines
+filename="../0.split_data/cytokine_list/cytokine_list.txt"
+
+# read all lines of the file to an array
+readarray -t cytokine_array < $filename
+# get all of the feature combinations
+# read all lines of the file to an array
+readarray -t feature_combination_keys < $feature_combination_key_file
+
+
+jupyter nbconvert --to=script --FilesWriter.build_directory=./scripts/ ./notebooks/*.ipynb
+
+shuffles=( True False )
+cell_types=( SHSY5Y PBMC )
+
+# get the first element of the array for testing
+cytokine_array=${cytokine_array[0]}
+feature_combination_keys=${feature_combination_keys[0]}
+
+for cell_type in "${cell_types[@]}"
+do
+    for shuffle in "${shuffles[@]}"
+    do
+        for feature_combination in "${feature_combination_keys[@]}"
+        do
+            for cytokine in "${cytokine_array[@]}"
+            do
+                echo "$cell_type $shuffle $feature_combination $cytokine"
+
+                # get the number of jobs for the user
+                number_of_jobs=$(squeue -u $USER | wc -l)
+                while [ $number_of_jobs -gt 2 ]; do
+                    sleep 5s
+                    number_of_jobs=$(squeue -u $USER | wc -l)
+                done
+
+                sbatch train_regression_call.sh $cell_type $shuffle $feature_combination $cytokine
+            done
+        done
+    done
+done
+
+
+# sbatch train_regression_call.sh $cell_type $shuffle $feature_combination
